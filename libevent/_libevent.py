107f5bebe4c2d65dde36ab370d4042ed2543f531
lins05 <linshuai2012@gmail.com>
2013-10-15 11:42:41 +0800

    fixed import .so in  __bootstrap__

--- libevent/_libevent.py
+++ libevent/_libevent.py
@@ -1,9 +1,12 @@
 def __bootstrap__():
-   global __bootstrap__, __loader__, __file__
-   import sys, pkg_resources, imp, platform
-   __file__ = pkg_resources.resource_filename(__name__,'libevent-py%s-%s-%s%s' % (sys.version[:3],sys.platform,platform.machine(),imp.get_suffixes()[0][0]))
-   __loader__ = None; del __bootstrap__, __loader__
-   imp.load_dynamic(__name__,__file__)
-   
+    global __bootstrap__, __loader__, __file__
+    import sys, pkg_resources, imp, platform
+    if 'x86_64' in platform.machine():
+        machine = 'x86_64'
+    else:
+        machine = 'i386'
+    __file__ = pkg_resources.resource_filename(__name__,'libevent-py%s-%s-%s.so' % (sys.version[:3],sys.platform,machine))
+    __loader__ = None; del __bootstrap__, __loader__
+    imp.load_dynamic(__name__,__file__)
 
 __bootstrap__()
